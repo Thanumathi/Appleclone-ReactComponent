7a04945734e755936807990f5fbefcf4ee1bd85b
Thanumathi <pthanumathi@gmail.com>
2025-09-30 15:09:03 +0530

    fix: updated Header component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ const header = () => {
   return (
    <header className="h-[800px] w-full bg-cover bg-center text-white text-center p-10" style={{ 
             backgroundImage:`url(${backgroundImage})`,
-          }}> 
+          }}>
            <h1 className='text-2xl md:text-3xl pt-4'>iPhone 17 Pro</h1>
            <h4 className='text-2xs md:text-xl'>All Out Pro</h4>
            <button className='border-0 px-4 py-2 mt-2 rounded-4xl bg-[#0077ED]'>LearnMore</button>
